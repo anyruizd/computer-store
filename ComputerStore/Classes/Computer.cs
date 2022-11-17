97c5019ce5f9506555a2afcf0d4e03e5147065e3
anyruizd <anyruizd@gmail.com>
2022-11-17 14:06:33 -0500

    Computer class

--- ComputerStore/Classes/Computer.cs
+++ ComputerStore/Classes/Computer.cs
@@ -1,3 +1,85 @@
 class Computer {
+    private static int computerNumber = 0;
     public string holi = "Holi";
+    private string brand;
+    private string model;
+    private long serialNumber;
+    private double price;
+
+    public Computer() {
+        brand = "";
+        model = "";
+        serialNumber = 0;
+        price = 0;
+        computerNumber++;
+    }
+
+    public Computer(string b, string m, long sn, double p) {
+        brand = b;
+        model = m;
+        serialNumber = sn;
+        price = p;
+        computerNumber++;
+    }
+
+    public void setBrand(string b){
+        brand = b;
+    }
+
+    public string getBrand(){
+        return brand;
+    }
+
+    public void setModel(string m){
+        model = m;
+    }
+
+    public string getModel(){
+        return model;
+    }
+
+    public void setSerialNumber(long sn){
+        if(sn < 0) {
+            serialNumber = sn;
+        } else {
+            Console.WriteLine("Error! Serial number should not be negative");
+        }
+    }
+
+    public long getSerialNumber(){
+        return serialNumber;
+    }
+
+    public void setPrice(double p){
+        if(p < 0) {
+            price = p;
+        } else {
+            Console.WriteLine("Error! Price should not be negative");
+        }
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int findNumberOfCreatedComputers () {
+        return computerNumber;
+    }
+
+    public void showComputer() {
+        Console.WriteLine($"-------- Computer ----------");
+        Console.WriteLine($"Brand: {brand}");
+        Console.WriteLine($"Model: {model}");
+        Console.WriteLine($"Serial Number: {serialNumber}");
+        Console.WriteLine($"Price: {price}");
+        Console.WriteLine("----------------------------");
+    }
+
+    public Boolean isEqualTo(Computer c) {
+        if(this.brand == c.getBrand() && this.price == c.getPrice() && this.model == c.getModel()){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- ComputerStore/Program.cs
+++ ComputerStore/Program.cs
@@ -6,7 +6,13 @@
 
 class Program {
     static void Main(string[] args) {
-        Computer c1 = new Computer();
-        Console.WriteLine(c1.holi);
+        Computer c1 = new Computer("apple", "macbook pro 13 inches", 1000001, 2500);
+        Console.WriteLine(c1.findNumberOfCreatedComputers());
+        Computer c2 = new Computer("apple", "macbook pro 13 inches", 1000002, 2500);
+        Console.WriteLine(c2.findNumberOfCreatedComputers());
+        c1.showComputer();
+        c2.showComputer();
+        string equal = c1.isEqualTo(c2) ? "" : "not ";
+        Console.WriteLine($"V1 is {equal}equal to V2");
     }
 }
