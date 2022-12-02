cfa33fe868d8fe947993fa56860c8328c0e75f28
anyruizd <anyruizd@gmail.com>
2022-12-01 22:33:29 -0500

    add comments

--- ComputerStore/Program.cs
+++ ComputerStore/Program.cs
@@ -13,11 +13,12 @@ class Program {
         Console.WriteLine("**********************************************************************");
         Console.WriteLine();
         string message = "Please enter the max amount of computers you want to have in your inventory: ";
-        int maxComputers = getInteger("Inventory", message);
+        int maxComputers = getInteger("Inventory", message); // verifies the input is valid
         Computer[] inventory = new Computer[maxComputers];
 
         Console.WriteLine();
         string option;
+        /************************************ Main menu ************************************/
         do {
             Console.WriteLine("----------------------------------------------------------------------");
             Console.WriteLine(" What do you want to do? ");
@@ -33,9 +34,11 @@ class Program {
 
             switch (option){
                 case "1":
+                    // enterNewComputers returns the updated value of maxComputers
                     maxComputers = enterNewComputers(maxComputers, inventory);
                     break;
                 case "2":
+                    // changeInformation returns the updated value of maxComputers
                     maxComputers = changeInformation(maxComputers, inventory);
                     break;
                 case "3":
@@ -149,13 +152,18 @@ class Program {
         }
     }
 
+    /*
+        getInteger receives an element and a message and returns an integer when it's valid,
+        the element is the string that represents what we are entering: inventory, computer number,  
+        the message is explaining the user what input we are expecting.
+    */
     static int getInteger(string element, string message){
         int number = -1;
 
         do {
             Console.Write(message);
             bool isValid = Int32.TryParse(Console.ReadLine(), out number);
-            if (!isValid || number < 1){
+            if (!isValid || number < 1){ // Does not accept a number smaller than 1
                 Console.WriteLine();
                 Console.WriteLine($"*** ERROR *** Invalid input! {element} should be an integer bigger than 0.");
                 Console.WriteLine();
@@ -166,6 +174,11 @@ class Program {
         return number;
     }
 
+    /*
+        getLong receives an element and a message and returns a long when it's valid,
+        the element is the string that represents what we are entering: serial number 
+        the message is explaining the user what input we are expecting.
+    */
     static long getLong(string element, string message){
         long number = -1;
 
@@ -183,6 +196,11 @@ class Program {
         return number;
     }
 
+    /*
+        getDouble receives an element and a message and returns a double when it's valid,
+        the element is the string that represents what we are entering: price 
+        the message is explaining the user what input we are expecting
+    */
     static double getDouble(string element, string message){
         double number = -1;
 
@@ -200,6 +218,10 @@ class Program {
         return number;
     }
 
+    /*
+        enterNewComputers receives the max number of computers available and the inventory,
+        and returns the updated number of max computers
+    */
     static int enterNewComputers(int maxComputers, Computer [] inventory) {
         bool validPwd = false;
         validPwd = validatePassword();
@@ -209,7 +231,7 @@ class Program {
         }
         string message = "How many computers do you want to enter?: ";
         int computersToEnter = getInteger("Computer", message);
-        if(computersToEnter > maxComputers) {
+        if(computersToEnter > maxComputers) { // we can no enter more computers than the max allowed
             Console.WriteLine();
             Console.WriteLine($"*** ERROR *** You can only enter {maxComputers} computers.");
             Console.WriteLine();
@@ -227,36 +249,42 @@ class Program {
         }
         return maxComputers;
     }
-    static int changeInformation(int maxComputers, Computer [] inventory) {
-        bool validPwd = false;
-        validPwd = validatePassword();
 
-        if(!validPwd) {
-            return maxComputers;
-        }
+    /*
+        changeInformation receives the max number of computers available and the inventory,
+        and returns the updated number of max computers
+    */
+    static int changeInformation(int maxComputers, Computer [] inventory) {
         int enteredComputers = Computer.findNumberOfCreatedComputers();
-        if(enteredComputers == 0 ) {
+        if(enteredComputers == 0 ) { // first we validate if we have computers in the inventory
             Console.WriteLine();
             Console.WriteLine("*** ERROR *** There is no computers in the inventory! Please try adding one first.");
             Console.WriteLine();
+            return maxComputers; // if there is no computers to modify, the number of maxComputers wasn't updated
+        }
+
+        bool validPwd = false;
+        validPwd = validatePassword();
+
+        if(!validPwd) { // if the password is invalid, the number of maxComputers wasn't updated
             return maxComputers;
         }
 
-        string message = "Which computer number do you wish to update?: ";
+        string message = "Enter the number of the computer you wish to update: ";
         int computerNumber = getInteger("Computer", message);
 
-        if(computerNumber > enteredComputers) {
+        if(computerNumber > enteredComputers) { // If the computer does not exist, we give the option to the user to add a new computer
             Console.WriteLine();
             Console.WriteLine($"*** ERROR *** There is no computer with number {computerNumber}, would you like to add a new computer?");
             Console.WriteLine();
             Console.Write("Enter Y to enter a new computer, N to go back to the main menu: ");
             string choice = Console.ReadLine() ?? "";
-            if(choice == "Y" || choice == "y") {
-                if(enteredComputers == maxComputers) {
+            if(choice.ToLower() == "y") { // We accept Y or y, otherwise it returns to the main menu
+                if(enteredComputers == maxComputers) { // Check if the inventory is already full
                     Console.WriteLine();
                     Console.WriteLine("*** ERROR *** You cannot enter more computers.");
                     Console.WriteLine();
-                } else {
+                } else { // if there is space in the inventory we add the new single computer
                     Console.WriteLine();
                     Console.WriteLine($"Enter computer {enteredComputers + 1} information:");
                     Console.WriteLine();
@@ -272,13 +300,13 @@ class Program {
                 Console.WriteLine();
             }
             Console.WriteLine();
-        } else {
+        } else { // If the computer exists in the inventory
             Console.WriteLine();
             Console.WriteLine($"-------- Computer # {computerNumber} ----------");
             inventory[computerNumber-1].showComputer();
             Console.WriteLine();
             string choice = "";
-            do {
+            do { // ask for the information to change
                 Console.WriteLine("What information would you like to change? ");
                 Console.WriteLine("    1 -> Brand");
                 Console.WriteLine("    2 -> Model");
@@ -294,9 +322,13 @@ class Program {
         return maxComputers;
     }
 
+    /*
+        displayByBrand receives the inventory and shows the 
+        list of computers filtered by the entered brand
+    */
     static void displayByBrand(Computer [] inventory){
         int enteredComputers = Computer.findNumberOfCreatedComputers();
-        if(enteredComputers == 0 ) {
+        if(enteredComputers == 0 ) { // if there is no computers in the inventory it gets out
             Console.WriteLine();
             Console.WriteLine("*** ERROR *** There is no computers in the inventory! Please try adding one first.");
             Console.WriteLine();
@@ -310,22 +342,26 @@ class Program {
         Console.WriteLine();
         Boolean found = false;
         for (int i = 0; i < Computer.findNumberOfCreatedComputers(); i++){
-            if(inventory[i].getBrand() == brand) {
+            if((inventory[i].getBrand()).ToLower() == brand.ToLower()) { // filter by brand is case insensitive
                 found = true;
                 inventory[i].showComputer();
                 Console.WriteLine();
             }
         }
-        if(!found) {
+        if(!found) { // If no computer with the entered brand was found
             Console.WriteLine();
             Console.WriteLine("*** ERROR *** There is no computers in the inventory with this brand! Please try again.");
             Console.WriteLine();
         }
     }
 
+    /*
+        displayByPrice receives the inventory and shows the 
+        list of computers filtered by the entered price
+    */
     static void displayByPrice(Computer [] inventory){
         int enteredComputers = Computer.findNumberOfCreatedComputers();
-        if(enteredComputers == 0 ) {
+        if(enteredComputers == 0 ) { // if there is no computers in the inventory it gets out
             Console.WriteLine();
             Console.WriteLine("*** ERROR *** There is no computers in the inventory! Please try adding one first.");
             Console.WriteLine();
@@ -333,7 +369,7 @@ class Program {
         }
 
         string message = "Enter the price you want to search: ";
-        double price = getDouble("Price", message);
+        double price = getDouble("Price", message); // verifies the price entered is valid
         Console.WriteLine();
         Console.WriteLine($"-------- Computers with price {price} ----------");
         Console.WriteLine();
@@ -346,13 +382,17 @@ class Program {
             }
         }
 
-        if(!found) {
+        if(!found) {// If no computer with the entered price was found
             Console.WriteLine();
             Console.WriteLine("*** ERROR *** There is no computers in the inventory with this price! Please try again.");
             Console.WriteLine();
         }
     }
 
+    /*
+        saveComputerList receives the inventory and saves the list of computers
+        into the file specified in the path
+    */
     static void saveComputerList(Computer[] inventory){
         string myFilePath = @"./computer-list.txt";
         StreamWriter sw = new StreamWriter(myFilePath);
@@ -366,15 +406,17 @@ class Program {
         sw.Close();
     }
 
+    /*
+        showSavedList reads and shows into the console the list of computers saved into the file specified in the path
+    */
     static void showSavedList() {
-         string myFilePath = @"./computer-list.txt"; 
+        string myFilePath = @"./computer-list.txt"; 
         string[] lines = File.ReadAllLines(myFilePath);
- 
+        
+        Console.WriteLine();
         foreach (string line in lines)
         {
-            Console.WriteLine();
             Console.WriteLine(line);
-            Console.WriteLine();
         }
     }
 }
